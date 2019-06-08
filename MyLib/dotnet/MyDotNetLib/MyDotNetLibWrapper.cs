29c6801db999fb84959f6cb2d96f0b90f0a2312c
tebeco <tebeco@gmail.com>
2019-06-08 16:32:50 +0200

    simplify vcxproj for OutDir, less magic for CSharp

--- MyLib/dotnet/MyDotNetLib/MyDotNetLibWrapper.cs
+++ MyLib/dotnet/MyDotNetLib/MyDotNetLibWrapper.cs
@@ -4,7 +4,12 @@ namespace MyDotNetLib
 {
     internal static class MyDotNetLibWrapper
     {
+
+#if DEBUG
+        [DllImport(@"..\..\..\..\..\lib\Debug\x64\MYNATIVELIB.dll", EntryPoint = "getPointerSize", CallingConvention = CallingConvention.StdCall)]
+#else
         [DllImport(@"MYNATIVELIB.dll", EntryPoint = "getPointerSize", CallingConvention = CallingConvention.StdCall)]
+#endif
         public static extern int GetPointerSize();
     }
 }
