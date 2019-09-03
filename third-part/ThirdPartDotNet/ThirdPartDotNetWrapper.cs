2fbdc38b00b62193daba681bb2cf80aeda63b3b5
tebeco <tebeco@gmail.com>
2019-09-03 12:17:00 +0200

    remove specific #if directive for the wrapper

--- third-part/ThirdPartDotNet/ThirdPartDotNetWrapper.cs
+++ third-part/ThirdPartDotNet/ThirdPartDotNetWrapper.cs
@@ -4,13 +4,7 @@ namespace ThirdPartDotNet
 {
     internal static class ThirdPartDotNetWrapper
     {
-
-#if DEBUG
-        [DllImport(@"..\..\..\..\..\..\lib\Debug\x64\THIRDPARTNATIVE.dll", EntryPoint = "getPointerSize", CallingConvention = CallingConvention.StdCall)]
-        //[DllImport(@"..\..\..\..\..\..\lib\Debug\Win32\THIRDPARTNATIVE.dll", EntryPoint = "getPointerSize", CallingConvention = CallingConvention.StdCall)]
-#else
         [DllImport(@"THIRDPARTNATIVE.dll", EntryPoint = "getPointerSize", CallingConvention = CallingConvention.StdCall)]
-#endif
         public static extern int GetPointerSize();
     }
 }
